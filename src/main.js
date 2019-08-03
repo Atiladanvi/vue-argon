626b002d560392d47605301f967c81adddbe9c00
Atiladanvi <atila.danvi@outlook.com>
2019-08-03 01:31:03 -0300

    Add scrolling to nav and adjust code style

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import Argon from "./plugins/argon-kit";
+import VueScrollactive from 'vue-scrollactive';
 import './registerServiceWorker'
 
 Vue.config.productionTip = false;
 Vue.use(Argon);
+Vue.use(VueScrollactive);
+
 new Vue({
   router,
   render: h => h(App)
